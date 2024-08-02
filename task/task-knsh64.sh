37b039a50b9d6026531f5f0965fd8f56161ad882
Lup Yuen Lee <luppy@appkaki.com>
2024-08-02 18:21:29 +0800

    Trim to first 100000 bytes

--- task/task-knsh64.sh
+++ task/task-knsh64.sh
@@ -42,8 +42,8 @@ function test_nuttx {
     | tee /tmp/release-$BUILD_PREFIX.log \
     2>&1
 
-  ## Trim to first 1500 lines
-  head -n 1500 /tmp/release-$BUILD_PREFIX.log \
+  ## Trim to first 100000 bytes
+  head -c 100000 /tmp/release-$BUILD_PREFIX.log \
     >/tmp/release2-$BUILD_PREFIX.log
   mv /tmp/release2-$BUILD_PREFIX.log \
     /tmp/release-$BUILD_PREFIX.log
